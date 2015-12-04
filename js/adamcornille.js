2d7bc0c2e41fb649fd61eada5d6364711bdcb837
lafncow <lafncow@users.noreply.github.com>
2015-12-04 01:07:21 -0800

    totally going to AB test

--- js/adamcornille.js
+++ js/adamcornille.js
@@ -4,11 +4,17 @@ if( abTests ){
     console.log(abTests);
     var abTestVals = [];
     var abTest_gaIndex = 1;
+    var hiddenSet = [];
 
     // loop through active tests
-    for( i in abTests){
+    for( var i=0, ii=abTests.length;i<ii;i++ ){
     	abTestVals[i] = (Math.random() < 0.5 ? 1 : 0);
+        console.log('ab-'+abTests[i]+'-'+(abTestVals[i]?0:1));
     	// hide A/B alternative
-    	document.getElementsByClassName('ab-'+abTests[i]+'-'+(abTestVals[i]?0:1));
+        hiddenSet = document.getElementsByClassName('ab-'+abTests[i]+'-'+(abTestVals[i]?0:1));
+        for( var j=0, jj=hiddenSet.length;j<jj;j++ ){
+            hiddenSet[j].style.display = 'none';
+        }
+    	
     }
 }
